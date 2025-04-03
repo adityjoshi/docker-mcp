b693d6312190638a4fce55850cdfdcf37dc80697
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-04-03 15:15:31 +0530

    docker-mcp server

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	Port   string
+	APIKey string
+}
+
+func LoadConfig() *Config {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Println("Warning: API_KEY not set. Using default value for development.")
+		apiKey = "default-development-api-key"
+	}
+
+	return &Config{
+		Port:   port,
+		APIKey: apiKey,
+	}
+}
